5d87b8573d1b4d3caa664f6daf0d097ac0e2935e
Yahely <yahelyoseguera@hotmail.es>
2024-07-10 23:41:46 -0700

    logo-form

---
+++ script.js
@@ -0,0 +1,35 @@
+const expresion = {
+    email: /^[a-zA-Z0-9_.=-]+@[a-zA-z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    password: /^.{4,12}$/
+}
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
+const answer = document.getElementById("answer");
+const eyes = document.getElementById("eyes");
+
+/* Lock Color */
+
+password.addEventListener("mouseover", (e)=>{
+
+})
+
+/* Show Password */
+eyes.addEventListener("click", (e)=>{
+    password.type = "text";
+})
+
+/* Form */
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    if(!expresion.email.test(email.value)){
+        answer.innerHTML = "Email is incorrect";
+    } else if(!expresion.password.test(password.value)){
+        answer.innerHTML = "Password isn't correct";
+    } else{
+        form.reset();
+        answer.innerHTML = "Loading...";
+
+    }
+})
